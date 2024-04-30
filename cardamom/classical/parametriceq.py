c11f9679e02a4ee2033d4a61b01f83aa3bd1d16e
javanasse <julian.vanasse@gmail.com>
2024-04-30 15:11:05 -0400

    WIP: filter coefficients

---
+++ cardamom/classical/parametriceq.py
@@ -0,0 +1,130 @@
+import os
+from typing import Tuple, Iterable
+import scipy.signal
+import torch
+import numpy as np
+import scipy 
+import matplotlib.pyplot as plt
+import librosa
+
+"""
+Parametric Equalizer 
+
+A "series connection of first-and second-order shelving and peak
+filters, which are controlled independently" [1, p. 63]
+
+[1] Zölzer, Udo, ed. DAFX Digital Audio Effects. 2nd ed. Chichester, 
+    West Sussex, UK: John Wiley & Sons, 2011.
+"""
+
+def _parametric_constants(fs, fc, gain):
+    """
+    Constants for 2nd order peak and shelf filters.
+    See Table 2.3 in [1], p. 23.
+    """
+    V0 = 10.0**(gain/20)
+    K = np.tan(np.pi * fc / fs)
+    return K, V0
+
+def lowshelf(fs: int, fc: float, gain: float) -> Tuple[Iterable, Iterable]:
+    """
+    Coefficients for a biquad lowshelf filter.
+
+    Arguments:
+    fs (int): sampling rate (Hz)
+    fc (float): cutoff frequency (Hz)
+    gain (float): gain (dB)
+
+    Returns: 
+    b (list): feedforward coefficients
+    a (list): feedback coefficients
+
+    Example: 
+    >>> import scipy
+    >>> import matplotlib.pyplot as plt
+    >>> b, a = lowshelf(32000, 200, -6)
+    >>> w, h = scipy.signal.freqz(b, a)
+    >>> plt.plot(w, librosa.amplitude_to_db(h))
+
+    """
+
+    K, V0 = _parametric_constants(fs, fc, gain)
+
+    # low shelf
+    denom = (1 + np.sqrt(2)*K + K**2)
+    b0 = (1 + np.sqrt(2 * V0)*K + V0*K**2) / denom
+    b1 = 2*(V0 * K**2 - 1) / denom
+    b2 = (1 - np.sqrt(2*V0)*K + V0*K**2) / denom
+    a1 = 2*(K**2 - 1) / denom
+    a2 = (1 - np.sqrt(2)*K + K**2) / denom
+
+    return [b0, b1, b2], [1, a1, a2]
+
+def highshelf(fs: int, fc: float, gain: float) -> Tuple[Iterable, Iterable]:
+    """
+    Coefficients for a biquad highshelf filter.
+
+    Arguments:
+    fs (int): sampling rate (Hz)
+    fc (float): cutoff frequency (Hz)
+    gain (float): gain (dB)
+
+    Returns: 
+    b (list): feedforward coefficients
+    a (list): feedback coefficients
+
+    Example: 
+    >>> import scipy
+    >>> import matplotlib.pyplot as plt
+    >>> b, a = highshelf(32000, 200, -6)
+    >>> w, h = scipy.signal.freqz(b, a)
+    >>> plt.plot(w, librosa.amplitude_to_db(h))
+    """
+
+    K, V0 = _parametric_constants(fs, fc, gain)
+
+    # high shelf
+    denom = (1 + np.sqrt(2)*K + K**2)
+    b0 = (V0 + np.sqrt(2*V0)*K + K**2) / denom
+    b1 = 2*(K**2 - V0) / denom
+    b2 = (V0 - np.sqrt(2*V0)*K + K**2) / denom
+    a1 = 2*(K**2 - 1) / denom
+    a2 = (1 - np.sqrt(2)*K + K**2) / denom
+
+    return [b0, b1, b2], [1, a1, a2]
+
+def peak(fs: int, fc: float, gain: float) -> Tuple[Iterable, Iterable]:
+    """
+    Coefficients for a peak filter.
+
+    Arguments:
+    fs (int): sampling rate (Hz)
+    fc (float): cutoff frequency (Hz)
+    gain (float): gain (dB)
+
+    Returns: 
+    b (list): feedforward coefficients
+    a (list): feedback coefficients
+
+    Example: 
+    >>> import scipy
+    >>> import matplotlib.pyplot as plt
+    >>> b, a = peak(32000, 200, -6)
+    >>> w, h = scipy.signal.freqz(b, a)
+    >>> plt.plot(w, librosa.amplitude_to_db(h))
+    """
+
+fs = 8000
+fc = fs/4
+gain = -10 # gain in dB
+
+b, a = highshelf(fs, fc, gain)
+w, h = scipy.signal.freqz(b, a)
+plt.plot(w, librosa.amplitude_to_db(h))
+
+b, a = lowshelf(fs, fc, gain)
+w, h = scipy.signal.freqz(b, a)
+plt.plot(w, librosa.amplitude_to_db(h))
+
+plt.show()
+
