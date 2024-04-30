e1ccc58fc0c011b506a26db69fcd1a8c0dfed770
javanasse <julian.vanasse@gmail.com>
2024-04-30 17:08:42 -0400

    finished ParametricEqualizer object

--- cardamom/classical/parametriceq.py
+++ cardamom/classical/parametriceq.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 from typing import Tuple, Iterable
 import scipy.signal
 import torch
@@ -10,11 +11,16 @@ import librosa
 from utils import delta, fir2spectrum
 
 """
+TODO: replace `numpy` arrays with `torch` tensors
+"""
+
+"""
 Parametric Equalizer 
 
 A "series connection of first-and second-order shelving and peak
 filters, which are controlled independently" [1, p. 63]
 
+References:
 [1] Zölzer, Udo, ed. DAFX Digital Audio Effects. 2nd ed. Chichester, 
     West Sussex, UK: John Wiley & Sons, 2011.
 """
@@ -163,6 +169,29 @@ class ParametricEqualizer():
     Parametric equalizer consister of the following LTI systems in 
     series:
         Low Shelf -> Peak -> Peak ... -> Peak -> High Shelf
+    
+    Example:
+    >>> fs = 8000
+    >>> eq = ParametricEqualizer(sample_rate=fs, num_filters=3)
+    >>> eq.update_parameters(0, center=200, gain=10)
+    >>> eq.update_parameters(1, center=1000, gain=-10, q=10)
+    >>> eq.update_parameters(2, center=3000, gain=3)
+
+    >>> num_samples = 1024
+    >>> d = delta(shape=(1, num_samples))
+    >>> h = eq.process(d)
+
+    >>> fig, axs = plt.subplots(2, 1, sharex=True)
+    >>> H, f = fir2spectrum(h, sample_rate=fs)
+    >>> axs[0].plot(f, np.abs(H[0, :]))
+    >>> axs[0].set_title('Parametric EQ Curve')
+
+    >>> for filt in eq.filters:
+    >>>     h = filt.process(d)
+    >>>     H, f = fir2spectrum(h, sample_rate=fs)
+    >>>     axs[1].plot(f, np.abs(H[0, :]))
+    >>> axs[1].set_title('Subfilter Curves')
+    >>> plt.show()
     """
 
     def __init__(self, sample_rate: int, num_filters: int):
@@ -176,15 +205,30 @@ class ParametricEqualizer():
                                                num=self.num_filters)
         self.filters = list()
         for n, fc in enumerate(self.center_frequencies):
-            if n == 0:
-                b, a = lowshelf(self.sample_rate, fc, 0.0)
-            elif n == len(self.center_frequencies)-1:
-                b, a = highshelf(self.sample_rate, fc, 0.0)
-            else:
-                b, a = peak(self.sample_rate, fc, 0.0, np.sqrt(2)/2)
-            self.filters.append(Filter(b, a))
-
+            filt = self._get_coeffs_by_index(index=n, center=fc)
+            self.filters.append(filt)
     
+    def _get_coeffs_by_index(self, index: int, center: float=None, gain: float=None, q: float=None) -> Filter:
+        # assign defaults
+        if not center:
+            center = self.sample_rate/2
+        if not gain: 
+            gain = 0.0
+        if not (index > 0 and index < self.num_filters-1) and q:
+            warnings.warn(f"shelf filters at index=0 and index={self.num_filters-1} do not have a `q` parameter!")
+
+        if index == 0:
+            b, a = lowshelf(self.sample_rate, center, gain)
+        elif index == self.num_filters-1:
+            b, a = highshelf(self.sample_rate, center, gain)
+        else:
+            if not q:
+                q = np.sqrt(2)/2
+            b, a = peak(self.sample_rate, center, gain, q)
+        return Filter(b, a)
+
+    def update_parameters(self, index: int, center: float, gain: float, q: float=None):
+        self.filters[index] = self._get_coeffs_by_index(index, center, gain, q)
 
     def process(self, x: np.ndarray) -> np.ndarray:
         """
@@ -209,31 +253,35 @@ class ParametricEqualizer():
         
         return y
 
-num_samples = 1024
-fs = 8000
-eq = ParametricEqualizer(sample_rate=fs, num_filters=3)
+if __name__ == '__main__':
+    fs = 8000
+    eq = ParametricEqualizer(sample_rate=fs, num_filters=3)
+    eq.update_parameters(0, center=200, gain=10)
+    eq.update_parameters(1, center=1000, gain=-10, q=10)
+    eq.update_parameters(2, center=3000, gain=3)
 
-d = delta(shape=(1, num_samples))
-h = eq.process(d)
+    num_samples = 1024
+    d = delta(shape=(1, num_samples))
+    h = eq.process(d)
 
-fig, axs = plt.subplots(2, 1, sharex=True)
-H, f = fir2spectrum(h, sample_rate=fs)
-axs[0].plot(f, np.abs(H[0, :]))
-
-for filt in eq.filters:
-    h = filt.process(d)
+    fig, axs = plt.subplots(2, 1, sharex=True)
     H, f = fir2spectrum(h, sample_rate=fs)
+    axs[0].plot(f, np.abs(H[0, :]))
+    axs[0].set_title('Parametric EQ Curve')
 
-    axs[1].plot(f, np.abs(H[0, :]))
-plt.show()
+    for filt in eq.filters:
+        h = filt.process(d)
+        H, f = fir2spectrum(h, sample_rate=fs)
+        axs[1].plot(f, np.abs(H[0, :]))
+    plt.show()
 
-# fs = 8000
-# fc = fs/4
-# gain = -10 # gain in dB
+    # fs = 8000
+    # fc = fs/4
+    # gain = -10 # gain in dB
 
-# b, a = peak(fs, fc, gain, 10)
-# w, h = scipy.signal.freqz(b, a)
-# plt.plot(w, librosa.amplitude_to_db(h))
+    # b, a = peak(fs, fc, gain, 10)
+    # w, h = scipy.signal.freqz(b, a)
+    # plt.plot(w, librosa.amplitude_to_db(h))
 
-plt.show()
+    plt.show()
 
